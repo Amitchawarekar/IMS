09efa2e65dbaffd1b2cd576878873b82f7a40e3e
Amit <amit.chawarekar@gmail.com>
2021-11-03 11:10:16 +0530

    creted add_staff function

--- IMS/IMS_app/HodViews.py
+++ IMS/IMS_app/HodViews.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def admin_home(request):
     return render(request,'hod_templates/home_content.html')
+
+def add_staff(request):
+    return render(request, 'hod_templates/add_staff_template.html')
