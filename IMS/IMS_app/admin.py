f67b157d99592015cddf5186dfc130fca8cf8796
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:39:16 +0530

    created blankuser model class

--- IMS/IMS_app/admin.py
+++ IMS/IMS_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from institute_management_app.models import CustomUser
+
+
+class UserModel(UserAdmin):
+    pass
+
+#This is to register CustomUser Model 
+admin.site.register(CustomUser,UserModel)
