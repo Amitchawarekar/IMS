77dc98541edc21abf2cc7567d4e5116145d77343
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:53:16 +0530

    added login functions for emailbackend.py

--- IMS/IMS_app/views.py
+++ IMS/IMS_app/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+import datetime
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+
+from IMS_app.EmailBackEnd import EmailBackEnd
 # Create your views here.
 def showDemoPage(request):
     return render(request, "demo.html")
@@ -8,7 +15,14 @@ def ShowLoginPage(request):
     return render(request,'login_page.html')
 
 def doLogin(request):
-    if request.method != 'POST':
-        return HttpResponse("<h2>Method is not Allowe</h2>")
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        return HttpResponse("Email : " + request.POST.get("email") + "Password : " + request.POST.get("password"))
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
+        if user!=None:
+            login(request,user)
+            return HttpResponseRedirect("/admin_home")
+        else:
+
+            messages.error(request,"Invalid Login Details")
+            return HttpResponseRedirect("/")
