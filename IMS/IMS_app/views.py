fd836eddebb1a48b00e17637ec37b936a1629df4
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:48:16 +0530

    updated admin.py and views.py

--- IMS/IMS_app/views.py
+++ IMS/IMS_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 def showDemoPage(request):
     return render(request, "demo.html")
