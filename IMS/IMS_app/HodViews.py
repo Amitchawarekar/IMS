202d387c33a2d38c00c71e73494f51343ea8e543
Amit <amit.chawarekar@gmail.com>
2021-11-03 11:14:24 +0530

    added function for add_staff_save in HodViews.py

--- IMS/IMS_app/HodViews.py
+++ IMS/IMS_app/HodViews.py
@@ -1,7 +1,32 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from IMS_app.models import CustomUser,Staffs
 
 def admin_home(request):
     return render(request,'hod_templates/home_content.html')
 
 def add_staff(request):
     return render(request, 'hod_templates/add_staff_template.html')
+
+def add_staff_save(request):
+    if request.method != 'POST':
+        return HttpResponse("Method not allowed")
+    else:
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        try:
+            #Creating customuser
+            user = CustomUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=2)
+            user.staffs.address=address
+            user.save()
+            messages.success(request,"Successfully Added Staff")
+            return HttpResponseRedirect("/add_staff")
+        except:
+            messages.error(request,"Failed to Add Staff")
+            return HttpResponseRedirect("/add_staff")
