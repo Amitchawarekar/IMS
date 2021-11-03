fb579aec3d9be4d560d98e97e92b41a7c718bf55
Amit <amit.chawarekar@gmail.com>
2021-11-03 11:15:28 +0530

    aadded urls for add_staff and add_staff_save

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -27,6 +27,8 @@ urlpatterns = [
     path('logout_user', views.logout_user),
     path('doLogin',views.doLogin),
     path('admin_home',HodViews.admin_home),
-    path('add_staff',HodViews.add_staff)
+    path('add_staff',HodViews.add_staff),
+    path('add_staff',HodViews.add_staff),
+    path('add_staff_save',HodViews.add_staff_save),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
