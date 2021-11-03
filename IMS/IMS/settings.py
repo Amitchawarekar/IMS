60157abbacf4776458ad133747fd893623e96641
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:34:46 +0530

    added media and static urls into settings.py

--- IMS/IMS/settings.py
+++ IMS/IMS/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,6 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7)!z8hlw0n2w@41_s=4y6csfmtu82=ane*go+qbbebcqw@h8fe'
 
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
