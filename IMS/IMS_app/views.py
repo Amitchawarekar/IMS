e3251a4874e98882db9a607d106d800a13d2acfa
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:46:03 +0530

    created view for dologin

--- IMS/IMS_app/views.py
+++ IMS/IMS_app/views.py
@@ -6,3 +6,9 @@ def showDemoPage(request):
 
 def ShowLoginPage(request):
     return render(request,'login_page.html')
+
+def doLogin(request):
+    if request.method != 'POST':
+        return HttpResponse("<h2>Method is not Allowe</h2>")
+    else:
+        return HttpResponse("Email : " + request.POST.get("email") + "Password : " + request.POST.get("password"))
