d7e50f33bc73ad29c7fc95dc6bc68dff2008cc3c
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:35:10 +0530

    added view for demo.html

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from IMS_app import views
 
+from IMS import settings
 urlpatterns = [
+    path('demo',views.showDemoPage),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
