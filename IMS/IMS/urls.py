af9acdd7a74472e7f4bbf630a6fdbb7095f0d543
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:54:13 +0530

    created urls for get_user_details and logout

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -23,8 +23,8 @@ urlpatterns = [
     path('demo',views.showDemoPage),
     path('admin/', admin.site.urls),
     path('',views.ShowLoginPage),
-    # path('get_user_details', views.GetUserDetails),
-    # path('logout_user', views.logout_user),
+    path('get_user_details', views.GetUserDetails),
+    path('logout_user', views.logout_user),
     path('doLogin',views.doLogin),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
