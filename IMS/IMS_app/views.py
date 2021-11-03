c2100f5c6cb224c29fe667f4f36583a70d094918
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:41:22 +0530

    created functions for login_page

--- IMS/IMS_app/views.py
+++ IMS/IMS_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def showDemoPage(request):
     return render(request, "demo.html")
+
+def ShowLoginPage(request):
+    return render(request,'login_page.html')
