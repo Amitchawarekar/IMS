0515567f8aebeeeec037995c7c5989303807ae6d
Amit <amit.chawarekar@gmail.com>
2021-11-03 10:36:38 +0530

    created customuser for all users

--- IMS/IMS_app/models.py
+++ IMS/IMS_app/models.py
@@ -5,6 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+#custom user for all users
 class CustomUser(AbstractUser):
     user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
     user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
