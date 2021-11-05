2a4bb5ad7c071f6e7e1b5c8f4428d031fdcbcb4a
Amit <amit.chawarekar@gmail.com>
2021-11-05 15:09:51 +0530

    added edit_staff_template and func and url for it

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -40,6 +40,8 @@ urlpatterns = [
     path('manage_student',HodViews.manage_student),    
     path('manage_course',HodViews.manage_course),    
     path('manage_subject',HodViews.manage_subject),    
+    path('edit_staff/<str:staff_id>',HodViews.edit_staff),    
+    path('edit_staff_save',HodViews.edit_staff_save),    
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
--- IMS/IMS_app/HodViews.py
+++ IMS/IMS_app/HodViews.py
@@ -134,3 +134,35 @@ def manage_course(request):
 def manage_subject(request):
     subjects = Subjects.objects.all()
     return render(request, 'hod_templates/manage_subject_template.html',{'subjects':subjects})
+
+def edit_staff(request,staff_id):
+    staff =Staffs.objects.get(admin=staff_id)
+    return render(request, 'hod_templates/edit_staff_template.html',{'staff':staff})
+
+def edit_staff_save(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        staff_id=request.POST.get("staff_id")
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        email=request.POST.get("email")
+        username=request.POST.get("username")
+        address=request.POST.get("address")
+
+        try:
+            user=CustomUser.objects.get(id=staff_id)
+            user.first_name=first_name
+            user.last_name=last_name
+            user.email=email
+            user.username=username
+            user.save()
+
+            staff_model=Staffs.objects.get(admin=staff_id)
+            staff_model.address=address
+            staff_model.save()
+            messages.success(request,"Successfully Edited Staff")
+            return HttpResponseRedirect("/edit_staff/"+ staff_id)
+        except:
+            messages.error(request,"Failed to Edit Staff")
+            return HttpResponseRedirect("/edit_staff/"+ staff_id)
