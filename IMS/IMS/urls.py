56c722952005a60c10e142dd5505830772fbf4b1
Amit <amit.chawarekar@gmail.com>
2021-11-18 11:51:08 +0530

    created view and update attendance functionality in staff panel

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -54,8 +54,12 @@ urlpatterns = [
     #Staff URL Path
     path('staff_home',StaffViews.staff_home, name='staff_home'),
     path('staff_take_attendance',StaffViews.staff_take_attendance, name='staff_take_attendance'),
+    path('staff_update_attendance',StaffViews.staff_update_attendance, name='staff_update_attendance'),
     path('get_students',StaffViews.get_students, name='get_students'),
+    path('get_attendance_dates',StaffViews.get_attendance_dates, name='get_attendance_dates'),
+    path('get_attendance_student',StaffViews.get_attendance_student, name='get_attendance_student'),
     path('save_attendance_data',StaffViews.save_attendance_data, name='save_attendance_data'),
+    path('save_updateattendance_data',StaffViews.save_updateattendance_data, name='save_updateattendance_data'),
 
     #Student URL Path
     path('student_home',StudentViews.student_home, name='student_home'),
