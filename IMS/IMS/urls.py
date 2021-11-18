6823ee6e1c2725e5a2c055e63b7373ebada203fd
Amit <amit.chawarekar@gmail.com>
2021-11-18 09:44:49 +0530

    Added staff take attendance for student

--- IMS/IMS/urls.py
+++ IMS/IMS/urls.py
@@ -53,6 +53,9 @@ urlpatterns = [
 
     #Staff URL Path
     path('staff_home',StaffViews.staff_home, name='staff_home'),
+    path('staff_take_attendance',StaffViews.staff_take_attendance, name='staff_take_attendance'),
+    path('get_students',StaffViews.get_students, name='get_students'),
+    path('save_attendance_data',StaffViews.save_attendance_data, name='save_attendance_data'),
 
     #Student URL Path
     path('student_home',StudentViews.student_home, name='student_home'),
--- IMS/IMS_app/StaffViews.py
+++ IMS/IMS_app/StaffViews.py
@@ -1,4 +1,57 @@
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.core import serializers
+
+from IMS_app.models import SessionYearModel, Students, Subjects,  Attendance ,AttendanceReport
+
 
 def staff_home(request):
     return render(request,"staff_templates/staff_home_template.html")
+
+def staff_take_attendance(request):
+    subjects=Subjects.objects.filter(staff_id=request.user.id)
+    session_years=SessionYearModel.object.all()
+    return render(request,"staff_templates/staff_take_attendance_template.html",{"subjects":subjects,"session_years":session_years})
+
+@csrf_exempt
+def get_students(request):
+    subject_id=request.POST.get("subject")
+    session_duration=request.POST.get("session_duration")
+
+    subject=Subjects.objects.get(id=subject_id)
+    session_model=SessionYearModel.object.get(id=session_duration)
+    students=Students.objects.filter(course_id=subject.course_id,session_year_id=session_model)
+   
+    list_data=[]
+
+    for student in students:
+        data_small={"id":student.admin.id,"name":student.admin.first_name+" "+student.admin.last_name}
+        list_data.append(data_small)
+    return JsonResponse(json.dumps(list_data),content_type="application/json",safe=False)
+
+@csrf_exempt
+def save_attendance_data(request):
+    student_ids=request.POST.get("student_ids")
+    subject_id=request.POST.get("subject_id")
+    attendance_date=request.POST.get("attendance_date")
+    session_year_id=request.POST.get("session_year_id")
+
+    subject_model=Subjects.objects.get(id=subject_id)
+    session_model=SessionYearModel.object.get(id=session_year_id)
+    json_sstudent=json.loads(student_ids)
+    #print(data[0]['id'])
+
+
+    try:
+        attendance=Attendance(subject_id=subject_model,attendance_date=attendance_date,session_year_id=session_model)
+        attendance.save()
+
+        for stud in json_sstudent:
+             student=Students.objects.get(admin=stud['id'])
+             attendance_report=AttendanceReport(student_id=student,attendance_id=attendance,status=stud['status'])
+             attendance_report.save()
+        return HttpResponse("OK")
+    except:
+        return HttpResponse("ERR")
