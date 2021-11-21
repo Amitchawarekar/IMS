a97322edced72a845d9fb2fa612b9301c2741a9b
Amit <amit.chawarekar@gmail.com>
2021-11-21 13:23:41 +0530

    added student view attendance functionality

--- IMS/IMS_app/StudentViews.py
+++ IMS/IMS_app/StudentViews.py
@@ -1,4 +1,36 @@
+import datetime
+
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from IMS_app.models import Subjects, Students, Courses, CustomUser, Attendance, AttendanceReport
 
 def student_home(request):
     return render(request,"student_templates/student_home_template.html")
+
+def student_view_attendance(request):
+    student = Students.objects.get(admin=request.user.id)
+    courses = student.course_id
+    subjects = Subjects.objects.filter(course_id=courses)
+    return render(request,"student_templates/student_view_attendance_template.html",{'subjects': subjects})
+
+def student_view_attendance_post(request):
+    subject_id=request.POST.get("subject")
+    start_date=request.POST.get("start_date")
+    end_date=request.POST.get("end_date")
+
+    start_data_parse = datetime.datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_data_parse = datetime.datetime.strptime(end_date, "%Y-%m-%d").date()
+
+    subject_obj=Subjects.objects.get(id=subject_id)
+    user_object=CustomUser.objects.get(id=request.user.id)
+    stud_obj=Students.objects.get(admin=user_object)
+
+    attendance=Attendance.objects.filter(attendance_date__range=(start_data_parse,end_data_parse),subject_id=subject_obj)
+    attendance_reports=AttendanceReport.objects.filter(attendance_id__in=attendance,student_id=stud_obj)
+
+    return render(request,"student_templates/student_attendance_data.html",{"attendance_reports":attendance_reports})
+
+    
